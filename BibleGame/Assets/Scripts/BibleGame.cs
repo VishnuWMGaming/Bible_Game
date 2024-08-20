733c752e398a068526d972aca945ab9157c1b9b0
VishnuWMGaming <vishnu.raveendran@wmgamingstudioz.com>
2024-08-20 18:55:25 +0530

    Added the login UI

--- BibleGame/Assets/Scripts/BibleGame.cs
+++ BibleGame/Assets/Scripts/BibleGame.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 using UnityEngine.Events;
@@ -23,5 +24,26 @@ namespace BibleGame
     public static class Actions
     {
         public static Action<CanvasType> ChangePanelActions;
+    }
+
+   namespace Data
+    {
+        public static class AppData
+        {
+            public static LoginData loginData;
+        }
+
+        public  class LoginData
+        {
+           string email;
+           string password;
+
+            public  LoginData (string email, string password)
+            {
+                this.email = email;
+                this.password = password;
+            }
+        }
+
     }
 }
--- BibleGame/Assets/Scripts/UI/Panels/LoginPanel.cs
+++ BibleGame/Assets/Scripts/UI/Panels/LoginPanel.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using UnityEngine.UI;
-using BibleGame;
+using BibleGame.Data;
 using TMPro;
+using System.Text.RegularExpressions;
+using BibleGame;
 
 
 public class LoginPanel : MonoBehaviour
@@ -12,12 +14,16 @@ public class LoginPanel : MonoBehaviour
     [Header("UI Settings:")]
     [SerializeField] Button loginButton;
     [SerializeField] Button hideButton;
+    [SerializeField] Button signButton;
 
     [SerializeField] Sprite closeEye;
     [SerializeField] Sprite openEye;
 
     [SerializeField] TMP_InputField password_Inputfield;
+    [SerializeField] TMP_InputField email_Inputfield;
 
+
+    string error_message;
 
     private const string matchEmailPattern =
       @"^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
@@ -31,6 +37,8 @@ public class LoginPanel : MonoBehaviour
     private void OnEnable()
     {
         hideButton.onClick.AddListener(HideAction);
+        loginButton.onClick.AddListener(LoginAction);
+        signButton.onClick.AddListener(() => Actions.ChangePanelActions(CanvasType.signup));
     }
 
     /// <summary>
@@ -39,6 +47,8 @@ public class LoginPanel : MonoBehaviour
     private void OnDisable()
     {
         loginButton.onClick.RemoveAllListeners();
+        hideButton.onClick.RemoveAllListeners();
+        signButton.onClick.RemoveAllListeners();    
     }
 
 
@@ -48,5 +58,53 @@ public class LoginPanel : MonoBehaviour
         password_Inputfield.ForceLabelUpdate();
 
         hideButton.image.sprite = password_Inputfield.contentType == TMP_InputField.ContentType.Standard ? openEye : closeEye;
-    } 
+    }
+
+
+    private static bool ValidateEmail(string email)
+    {
+        if (email != null)
+            return Regex.IsMatch(email, matchEmailPattern);
+        else
+            return false;
+    }
+
+
+    private  bool ValidatePassword(string password)
+    {
+        bool isValid = false;   
+
+        if(password == null || password == string.Empty)
+        {
+            error_message = "Please enter the password";
+            isValid = false;
+        }
+        else if(password.Length < 8)
+        {
+            error_message = "Password length must be 8 character";
+        }
+        else
+            isValid = true;
+
+        return isValid; 
+    }
+
+
+    private void LoginAction()
+    {
+        var validEmail = ValidateEmail(email_Inputfield.text);
+        var validPassword = ValidatePassword(password_Inputfield.text); 
+        
+        if(!validEmail)
+        {
+            Debug.LogError("Enter the valid email");
+            return;
+        }
+
+        if (!validPassword)
+        {
+            Debug.LogError(error_message);
+            return;
+        }
+    }
 }
